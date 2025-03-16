4caab328a09a8cab5429355c718f72302709d181
CreepyTeabag <sovunja1712@gmail.com>
2025-03-16 13:25:04 +0300

    Finished 2.2.5

---
+++ todo/dist/todoItem.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TodoItem = void 0;
+/* export class TodoItem {
+  public id: number;
+  public task: string;
+  public complete: boolean = false;
+
+  public constructor(id: number, task: string, complete: boolean = false) {
+    this.id = id;
+    this.task = task;
+    this.complete = complete;
+  }
+
+  public printDetails(): void {
+    console.log(
+      `${this.id}\t${this.task} ${this.complete ? "\t(complete)" : ""}`
+    );
+  }
+} */
+class TodoItem {
+    id;
+    task;
+    complete;
+    constructor(id, task, complete = false) {
+        this.id = id;
+        this.task = task;
+        this.complete = complete;
+    }
+    printDetails() {
+        console.log(`${this.id}\t${this.task} ${this.complete ? "\t(complete)" : ""}`);
+    }
+}
+exports.TodoItem = TodoItem;
